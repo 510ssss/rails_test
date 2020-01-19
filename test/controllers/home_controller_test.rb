3a6a6d0302620e747839cab60bca8c3f4ca023a0
faith-goto <goto@faith.co.jp>
2020-01-19 14:09:01 +0900

    add: users

--- test/controllers/home_controller_test.rb
+++ test/controllers/home_controller_test.rb
@@ -1,9 +1,14 @@
 require 'test_helper'
 
 class HomeControllerTest < ActionDispatch::IntegrationTest
+  def setup
+    @base_title = "Ruby on Rails Tutorial Sample App"
+  end
+
   test "should get index" do
     get home_index_url
     assert_response :success
+    assert_select "title", @base_title
   end
 
 end
